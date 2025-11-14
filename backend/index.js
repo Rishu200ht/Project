4c9f501653b6ec7463f7a980cf0b904b8fb3cb37
Nilesh Agrahari <nagrahari539@gmail.com>
2025-11-14 20:53:26 +0530

    commit by nilesh

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors({
-  origin: "http://localhost:5000", // change to your frontend URL
+  origin: "*", // change to your frontend URL
   methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
+  // credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
